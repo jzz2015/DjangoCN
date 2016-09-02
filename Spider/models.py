9f95a11854e1ba2b7e0d7b7c5b1f22cfd09ad7be
zhouAndy <zhoucc2012@gmail.com>
2016-09-02 23:33:15 +0800

    add apps

--- Spider/models.py
+++ Spider/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# 目标站点
+class Dest(models.Model):
+    webname = models.CharField(max_length=50)
+    url = models.CharField(max_length=200)
+    # order_user = models.ForeignKey()
+
+
+# 抓取的新闻表
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=20)
+    post_date = models.DateTimeField()
+    content = models.TextField()
