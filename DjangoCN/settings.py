27b5dae48fd514461a5b908c92bc37ae60b6be62
zhouAndy <zhoucc2012@gmail.com>
2016-09-02 21:29:23 +0800

    add User, Spider apps

--- DjangoCN/settings.py
+++ DjangoCN/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'User',
+    'Spider',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'DjangoCN.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'root',
+        'PASSWORD': 'zhou347742',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
